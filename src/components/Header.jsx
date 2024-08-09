03062f16d568fd67bfdb849daace0f774512aa09
VaibhavVe <vaibhavverma0620@gmail.com>
2024-08-09 10:56:16 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+//import HighlightIcon from "@mui/icons-material/Highlight";
+import BookIcon from '@mui/icons-material/Book';
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BookIcon />
+        Diary
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
